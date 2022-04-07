49185f356f20431f6409c7ffd76494ae5e79c1ca
Tanner <Tanner@tleroy.com>
2022-04-06 21:14:02 -0400

    commit tree 0b74782a559840f3068046dcebeaed00f7c2a8d7

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// const findMatching = (arr, string) => {
+//    const newArray =[]
+//    const result = arr.filter(element => element === string);
+//    return result.name;
+// }
+
+function findMatching(listDrivers, nameDriver){
+    let newArr = listDrivers.filter( element => element.toUpperCase() === nameDriver.toUpperCase());
+    return newArr
+}
+
+function fuzzyMatch(listDrivers, nameDriver){
+    let newArr1 = listDrivers.filter(element => element[0] === nameDriver[0])
+    return newArr1
+}
+
+function matchName(listDrivers, nameDriver){
+    let newArr2 = listDrivers.filter( element => element.name === nameDriver)
+    return newArr2
+}
